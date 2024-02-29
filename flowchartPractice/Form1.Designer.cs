48e512718017f96a55b1deaac99229b61f80cba8
briasari <briasari660@gmail.com>
2024-02-29 14:35:51 -0500

    Add project files.

---
+++ flowchartPractice/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace flowchartPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.textLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(132, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(119, 47);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // textLabel
+            // 
+            this.textLabel.BackColor = System.Drawing.Color.Transparent;
+            this.textLabel.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLabel.ForeColor = System.Drawing.Color.White;
+            this.textLabel.Location = new System.Drawing.Point(-1, 207);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(387, 23);
+            this.textLabel.TabIndex = 1;
+            this.textLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(133, 389);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(119, 47);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.textLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ flowchartPractice/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace flowchartPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            Refresh();
+            textLabel.Text = "";
+
+            string text = "Hello World";
+
+            foreach (char c in text)
+            {
+                textLabel.Text += c;
+                Thread.Sleep(50);
+                textLabel.Refresh();
+            }
+
+            Thread.Sleep(3000);
+
+
+            this.BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(400);
+            
+            this.BackColor = Color.Black;
+            Refresh();
+            Thread.Sleep(400);
+
+            this.BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(400);
+
+            this.BackColor = Color.Black;
+            Refresh();
+            Thread.Sleep(400);
+
+
+            string text2 = "Press the exit button";
+            textLabel.Text = "";
+
+            foreach (char c in text2)
+            {
+                textLabel.Text += c;
+                Thread.Sleep(50);
+                textLabel.Refresh();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            exitButton.Visible = false;
+            startButton.Visible = false;
+            textLabel.Text = "";
+
+            string text = "Good Bye";
+
+            foreach (char c in text)
+            {
+                textLabel.Text += c;
+                Thread.Sleep(50);
+                textLabel.Refresh();
+            }
+            Thread.Sleep(2500);
+
+            Application.Exit();
+        }
+    }
+}
